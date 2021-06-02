263b30fcb273cd498a1745270c0706f02fd22e34
busdm@gmail.com <Teamiiko1313>
2021-06-02 17:26:12 +0300

    Инициализация. Создание первых файлов.

---
+++ src/test/java/Tests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Tests {
+
+    @Test
+    public void op() {
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").setValue("eroshenkoam/allure-example");
+        $(".header-search-input").pressEnter();
+        $(byAttribute("href","/eroshenkoam/allure-example")).click();
+        $(withText("Issues")).click();
+        $(withText("#68")).should(exist);
+
+    }
+}
